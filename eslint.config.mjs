dc1ef1dc69ab4e6ad74a11a141970f59a30fc568
wardaddy98 <suddath.gautam98@gmail.com>
2026-05-04 17:05:20 +0530

    husky mjs test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,20 @@
 import js from '@eslint/js';
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-                      {
+  {
     ignores: ['dist', 'node_modules'],
   },
   {
-    files:            
-       ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
     languageOptions: { globals: globals.node },
     extends: ['js/recommended'],
-    rules: {
-    },
+    rules: {},
   },
   tseslint.configs.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
 ]);
